32f37db4630c46bdf3a9eece343f2b82e5e9e306
Kitsune <1852139224@qq.com>
2022-06-09 22:10:52 +0800

    add 新增了课程大纲中章节添加和修改功能

--- src/api/edu/chapter.js
+++ src/api/edu/chapter.js
@@ -7,6 +7,40 @@ export default {
       url: `/eduservice/edu-chapter/getChapterVideo/${courseId}`,
       method: 'get'
     })
+  },
+
+  //修改章节内容
+  updateChapter(chapter){
+    return request({
+      url: '/eduservice/edu-chapter/updateChapter',
+      method: 'post',
+      data: chapter
+    })
+  },
+
+  //删除章节
+  deleteChapterById(chapterId){
+    return request({
+      url: '/eduservice/edu-chapter/deleteChapter/' + chapterId,
+      method: 'delete'
+    })
+  },
+
+  //根据id查询章节
+  getChapterById(chapterId) {
+    return request({
+      url: `/eduservice/edu-chapter/getChapterById/${chapterId}`,  //换种写法，换个心情 笑
+      method: 'get'
+    })
+  },
+
+  //添加章节
+  addChapter(chapter) {
+    return request({
+      url: '/eduservice/edu-chapter/addChapter',
+      method: 'post',
+      data: chapter
+    })
   }
 
 }
--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -9,17 +9,25 @@
       <el-step title="最终发布"/>
     </el-steps>
 
+    <el-button type="text" @click="openChapterDialog">添加章节</el-button>
+
     <ul class="chanpterList">
       <!--      先遍历章节-->
       <li v-for="chapter in chapterVideoList" :key="chapter.id">
         <p>
           {{ chapter.title }}
+          <span class="acts">
+<!--                <el-button type="text">添加课时</el-button>-->
+                <el-button style="" type="text" @click="openEditChapter(chapter.id)">编辑</el-button>
+                <el-button type="text">删除</el-button>
+            </span>
         </p>
         <ul class="chanpterList videoList">
           <!--然后遍历小节-->
           <li v-for="video in chapter.children" :key="video.id">
             <p>
               {{ video.title }}
+
             </p>
           </li>
         </ul>
@@ -34,6 +42,24 @@
       </el-form-item>
 
     </el-form>
+
+    <!-- 添加和修改章节表单 -->
+
+    <el-dialog :visible.sync="dialogChapterFormVisible" title="添加章节">
+      <el-form :model="chapter" label-width="120px">
+        <el-form-item label="章节标题">
+          <el-input v-model="chapter.title"/>
+        </el-form-item>
+        <el-form-item label="章节排序">
+          <el-input-number v-model="chapter.sort" :min="0" controls-position="right"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogChapterFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveOrUpdate">确 定</el-button>
+      </div>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -46,6 +72,13 @@ export default {
       saveBtnDisabled: false,
       courseId: '', //课程id
       chapterVideoList: [],
+      dialogChapterFormVisible: false, //章节弹框
+      chapter: {
+        title: '',
+        sort: 0,
+        courseId: this.courseId
+
+      },
 
     }
   },
@@ -60,6 +93,50 @@ export default {
 
   },
   methods: {
+    //修改章节
+    updateChapter(){
+      this.chapter.courseId = this.courseId
+      chapter.updateChapter(this.chapter)
+        .then(response => {
+          //关闭弹窗
+          this.dialogChapterFormVisible = false
+          //提示信息
+          this.$message({
+            type: 'success',
+            message: '修改章节信息成功',
+          })
+          //刷新页面
+          this.getAllChapterVideo()
+        })
+    },
+
+    //添加章节
+    addChapter(){
+      this.chapter.courseId = this.courseId
+      chapter.addChapter(this.chapter)
+        .then(response => {
+          //关闭弹窗
+          this.dialogChapterFormVisible = false
+          //提示信息
+          this.$message({
+            type: 'success',
+            message: '添加成功',
+          })
+          //刷新页面
+          this.getAllChapterVideo()
+        })
+    },
+
+    saveOrUpdate(){
+
+      if(!this.chapter.id){
+        this.addChapter()
+      }else {
+        this.updateChapter()
+      }
+
+    },
+
     //根据课程id查询章节和小节的数据列表
     getAllChapterVideo() {
       chapter.getChapterVideoByCourseId(this.courseId)
@@ -77,6 +154,20 @@ export default {
     previous() {
       //跳转到第一步
       this.$router.push({path: '/course/info/' + this.courseId})
+    },
+    openChapterDialog(){
+      this.chapter.title = ''
+      this.chapter.sort = 0
+      this.dialogChapterFormVisible = true
+    },
+    openEditChapter(chapterId){
+      //获取数据回显
+      chapter.getChapterById(chapterId)
+        .then(response => {
+          this.chapter = response.data.chapter
+        })
+      //弹框
+      this.dialogChapterFormVisible = true
     }
   }
 }
@@ -85,56 +176,60 @@ export default {
 <style scoped>
 
 .chanpterList {
-   position: relative;
-   list-style: none;
-   margin: 0;
-   padding: 0;
+  position: relative;
+  list-style: none;
+  margin: 0;
+  padding: 0;
 
 }
 
 
 .chanpterList li {
-   position: relative;
+  position: relative;
 
 }
 
 
 .chanpterList p {
-   float: left;
-   font-size: 20px;
-   margin: 10px 0;
-   padding: 10px;
-   height: 70px;
-   line-height: 50px;
-   width: 100%;
-   border: 1px solid #DDD;
+  float: left;
+  font-size: 20px;
+  margin: 10px 0;
+  padding: 10px;
+  height: 70px;
+  line-height: 50px;
+  width: 100%;
+  border: 1px solid #DDD;
+  /*用对定位来定位p标签*/
+  position: relative;
 
 }
 
 
 .chanpterList .acts {
-   float: right;
-   font-size: 14px;
+  float: right;
+  font-size: 14px;
+  /*同样使用相对定位，index设置为1避免这个元素被p标签遮住*/
+  position: relative;
+  z-index: 1;
 
 }
 
 
-
 .videoList {
-   padding-left: 50px;
+  padding-left: 50px;
 
 }
 
 
 .videoList p {
-   float: left;
-   font-size: 14px;
-   margin: 10px 0;
-   padding: 10px;
-   height: 50px;
-   line-height: 30px;
-   width: 100%;
-   border: 1px dotted #DDD;
+  float: left;
+  font-size: 14px;
+  margin: 10px 0;
+  padding: 10px;
+  height: 50px;
+  line-height: 30px;
+  width: 100%;
+  border: 1px dotted #DDD;
 
 }
 
